fa9f9009117a87149e2efbd74ea9856b036d1714
chan-PC\chan <chancmg007@gmail.com>
2017-06-30 15:28:47 +0530

    added samples

---
+++ sample.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by chan on 6/30/2017.
+ */
+fun main(args: Array<String>) {
+    var lettergrade: Char = 'A'
+    println("A is char ${lettergrade is Char}")
+
+    println("3.4 to Integer ${3.4.toInt()}")
+    println("A to Integer" + ('A'.toInt()))
+    println("65 to char ${65.toChar()}")
+}
